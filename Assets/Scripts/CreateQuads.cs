2dc44c3ad9a1d70554b48a3d405697e03bf5d52a
Anton Le Prevost-Smith <Alps709>
2019-03-26 10:50:18 +1300

    Added quad creation to next be used with block creation.

--- Assets/Scripts/CreateQuads.cs
+++ Assets/Scripts/CreateQuads.cs
@@ -5,6 +5,16 @@ using UnityEngine;
 public class CreateQuads : MonoBehaviour {
 
 	public Material cubeMaterial;
+
+	enum CubeSide
+	{
+		BOTTOM,
+		TOP,
+		LEFT,
+		RIGHT,
+		FRONT,
+		BACK
+	};
 	
 	void CreateQuad()
 	{
@@ -18,13 +28,13 @@ public class CreateQuads : MonoBehaviour {
 		Vector2[] uvs = new Vector2[4];
 		int[] triangles = new int[6];
 
-		//all possible UVs
+		//Create all needed UVs for the quad
 		Vector2 uv00 = new Vector2( 0f, 0f );
 		Vector2 uv10 = new Vector2( 1f, 0f );
 		Vector2 uv01 = new Vector2( 0f, 1f );
 		Vector2 uv11 = new Vector2( 1f, 1f );
 
-		//all possible vertices
+		//Create all needed vertices for the quad
 		Vector3 p0 = new Vector3( -0.5f,  -0.5f,  0.5f );
 		Vector3 p1 = new Vector3(  0.5f,  -0.5f,  0.5f );
 		Vector3 p2 = new Vector3(  0.5f,  -0.5f, -0.5f );
@@ -35,26 +45,36 @@ public class CreateQuads : MonoBehaviour {
 		Vector3 p7 = new Vector3( -0.5f,   0.5f, -0.5f );
 
 		
+		//Put the created values in the arrays
 		vertices = new Vector3[] {p4, p5, p1, p0};
 		normals = new Vector3[] {Vector3.forward, 
 								 Vector3.forward, 
 								 Vector3.forward, 
 								 Vector3.forward};
 		
+		//Put the created values in the arrays
 		uvs = new Vector2[] {uv11, uv01, uv00, uv10};
 		triangles = new int[] {3, 1, 0, 3, 2, 1};
 
+		//Set the meshes values to the ones we just created
 		mesh.vertices = vertices;
 		mesh.normals = normals;
 		mesh.uv = uvs;
 		mesh.triangles = triangles;
 		 
+		//Recalculates the bounding box that encompasses the mesh,
+		//it's a local spacebox that sits inline with the axis and is used for rendering
 		mesh.RecalculateBounds();
 		
+		//Create a new quad object and parent that to our quad
 		GameObject quad = new GameObject("quad");
 	    quad.transform.parent = this.gameObject.transform;
+	    
+	    //Add new meshfilter to our created quad and sets its mesh to the one we just created
      	MeshFilter meshFilter = (MeshFilter) quad.AddComponent(typeof(MeshFilter));
 		meshFilter.mesh = mesh;
+		
+		//Add renderer component so it can be rendered, also set the material
 		MeshRenderer renderer = quad.AddComponent(typeof(MeshRenderer)) as MeshRenderer;
 		renderer.material = cubeMaterial;
 	}
