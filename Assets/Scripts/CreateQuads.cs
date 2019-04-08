7d898ba1844bc81d1d589814d8e5f949756c43e7
Anton Le Prevost-Smith <Alps709>
2019-04-09 11:16:30 +1200

    Finished creating cube from multiple quads.

--- Assets/Scripts/CreateQuads.cs
+++ Assets/Scripts/CreateQuads.cs
@@ -8,8 +8,8 @@ public class CreateQuads : MonoBehaviour {
 
 	enum CubeSide
 	{
-		BOTTOM,
 		TOP,
+		BOTTOM,
 		LEFT,
 		RIGHT,
 		FRONT,
@@ -47,13 +47,6 @@ public class CreateQuads : MonoBehaviour {
 		//Switch that chooses which quad it needs to make
 		switch(side)
         		{
-        			case CubeSide.BOTTOM:
-        				vertices = new Vector3[] {p0, p1, p2, p3};
-        				normals = new Vector3[] {Vector3.down, Vector3.down, 
-        											Vector3.down, Vector3.down};
-        				uvs = new Vector2[] {uv11, uv01, uv00, uv10};
-        				triangles = new int[] { 3, 1, 0, 3, 2, 1};
-        			break;
         			case CubeSide.TOP:
         				vertices = new Vector3[] {p7, p6, p5, p4};
         				normals = new Vector3[] {Vector3.up, Vector3.up, 
@@ -61,6 +54,15 @@ public class CreateQuads : MonoBehaviour {
         				uvs = new Vector2[] {uv11, uv01, uv00, uv10};
         				triangles = new int[] {3, 1, 0, 3, 2, 1};
         			break;
+        			
+                    case CubeSide.BOTTOM:
+	                    vertices = new Vector3[] {p0, p1, p2, p3};
+	                    normals = new Vector3[] {Vector3.down, Vector3.down, 
+		                    Vector3.down, Vector3.down};
+	                    uvs = new Vector2[] {uv11, uv01, uv00, uv10};
+	                    triangles = new int[] { 3, 1, 0, 3, 2, 1};
+	                break;
+                    
         			case CubeSide.LEFT:
         				vertices = new Vector3[] {p7, p4, p0, p3};
         				normals = new Vector3[] {Vector3.left, Vector3.left, 
@@ -68,6 +70,7 @@ public class CreateQuads : MonoBehaviour {
         				uvs = new Vector2[] {uv11, uv01, uv00, uv10};
         				triangles = new int[] {3, 1, 0, 3, 2, 1};
         			break;
+        			
         			case CubeSide.RIGHT:
         				vertices = new Vector3[] {p5, p6, p2, p1};
         				normals = new Vector3[] {Vector3.right, Vector3.right, 
@@ -75,6 +78,7 @@ public class CreateQuads : MonoBehaviour {
         				uvs = new Vector2[] {uv11, uv01, uv00, uv10};
         				triangles = new int[] {3, 1, 0, 3, 2, 1};
         			break;
+        			
         			case CubeSide.FRONT:
         				vertices = new Vector3[] {p4, p5, p1, p0};
         				normals = new Vector3[] {Vector3.forward, Vector3.forward, 
@@ -82,6 +86,7 @@ public class CreateQuads : MonoBehaviour {
         				uvs = new Vector2[] {uv11, uv01, uv00, uv10};
         				triangles = new int[] {3, 1, 0, 3, 2, 1};
         			break;
+        			
         			case CubeSide.BACK:
         				vertices = new Vector3[] {p6, p7, p3, p2};
         				normals = new Vector3[] {Vector3.back, Vector3.back, 
@@ -114,9 +119,57 @@ public class CreateQuads : MonoBehaviour {
 		renderer.material = cubeMaterial;
 	}
 
+	void CombineQuads()
+	{
+		///Combine all children meshes
+		
+		//Create arrays for the mesh filters and mesh instances that need to be combined
+		MeshFilter[] meshFilters = GetComponentsInChildren<MeshFilter>();
+		CombineInstance[] meshesToCombine = new CombineInstance[meshFilters.Length];
+		
+		for (int i = 0; i < meshFilters.Length; i++)
+		{
+			//Set the array to have the mesh values
+			meshesToCombine[i].mesh = meshFilters[i].sharedMesh;
+			
+			//Convert local vertices co-ords to worldspace co-ords
+			meshesToCombine[i].transform = meshFilters[i].transform.localToWorldMatrix;
+		}
+		
+		//Create new mesh on the parent object
+		MeshFilter mf = (MeshFilter) this.gameObject.AddComponent(typeof(MeshFilter));
+		mf.mesh = new Mesh();
+		
+		//Add combined meshes of the children to the parent's mesh
+		mf.mesh.CombineMeshes(meshesToCombine);
+		
+		//Create renderer for the parent object (which is now all the combined quad meshes)
+		MeshRenderer renderer = this.gameObject.AddComponent(typeof(MeshRenderer)) as MeshRenderer;
+		renderer.material = cubeMaterial;
+		
+		//delete all the original uncombined quads
+		foreach (Transform quad in this.transform)
+		{
+			Destroy(quad.gameObject);
+		}
+	}
+
+	void CreateCube()
+	{
+		CreateQuad(CubeSide.TOP);
+		CreateQuad(CubeSide.BOTTOM);
+		CreateQuad(CubeSide.LEFT);
+		CreateQuad(CubeSide.RIGHT);
+		CreateQuad(CubeSide.FRONT);
+		CreateQuad(CubeSide.BACK);
+		
+	}
+
 	// Use this for initialization
-	void Start () {
-		CreateQuad();
+	void Start () 
+	{
+		CreateCube();
+		CombineQuads();
 	}
 	
 	// Update is called once per frame
